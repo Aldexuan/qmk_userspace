26e0453baf4b17180747d197cbc4038edf155a80
Sieben <1029452734@qq.com>
2024-09-29 14:00:57 +0800

    test1

--- keyboards/charybdis/4x6/v2/splinky_2/config.h
+++ keyboards/charybdis/4x6/v2/splinky_2/config.h
@@ -18,19 +18,20 @@
 #pragma once
 
 /* Handedness. */
-#define MASTER_RIGHT
+//#define MASTER_RIGHT
 
-// To use the handedness pin, resistors need to be installed on the adapter PCB.
-// If so, uncomment the following code, and undefine MASTER_RIGHT above.
-// #define SPLIT_HAND_PIN GP13
-// #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
+ #define SPLIT_HAND_PIN GP15
+ #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
+
+/* VBUS detection. */
+#define USB_VBUS_PIN GP19
 
 /* SPI & PMW3360 settings. */
 #define SPI_DRIVER SPID0
-#define SPI_SCK_PIN GP18
+#define SPI_SCK_PIN GP22
 #define SPI_MISO_PIN GP20
-#define SPI_MOSI_PIN GP19
-#define POINTING_DEVICE_CS_PIN GP14
+#define SPI_MOSI_PIN GP23
+#define PMW33XX_CS_PIN GP16
 
 /* Reset. */
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
